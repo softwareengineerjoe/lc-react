cc5c51d179f6bac7c19a380df2baaa893a242088
Joe-Pisopay <maranan.mcjo@gmail.com>
2023-01-03 11:45:41 +0800

    Making use of state

--- src/components/Another.jsx
+++ src/components/Another.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Another(props) {
   return (
-    <div>Another Component, {props.name}</div>
+    <div>
+      Another Component, { props.name }
+    </div>
   )
 }
